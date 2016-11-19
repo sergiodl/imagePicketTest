356f46db1beee9295119ef98e939bf4d35e8da67
Damo de Lemos <lemoss@amazon.com>
2016-11-19 16:47:42 -0600

    First commit

---
+++ imagePicker/imagePicker/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  imagePicker
+//
+//  Created by Damo de Lemos, Sergio on 11/19/16.
+//  Copyright © 2016 Damo de Lemos, Sergio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+
+    @IBAction func butonClick(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        self.present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
